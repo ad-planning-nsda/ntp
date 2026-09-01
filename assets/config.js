01c54182d4ec7974e39cf78b18e58238d2d98172
ad-planning-nsda <ad.planning.nsda@gmail.com>
2026-09-01 16:35:52 +0600

    Fifth Upload

--- assets/config.js
+++ assets/config.js
@@ -2,4 +2,4 @@
 // PASTE your deployed Google Apps Script Web App URL below.
 // It looks like: https://script.google.com/macros/s/XXXXXXXX/exec
 // ============================================================
-window.API_URL = "https://script.google.com/macros/s/AKfycbxmqW_hPf_XY6cBF7oQ6eeQm3NOKk40bG_3jd-IZOMIfFPimXUjuSzUhlWQLMaCybX2Eg/exec";
+window.API_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
